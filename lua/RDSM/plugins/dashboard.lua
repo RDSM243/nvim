fdac3d466001bce35addb9373e00bb767cdd9043
RDSM243 <didicomendes13@gmail.com>
2023-12-14 16:55:57 -0300

    More Dashboard Improvements

--- lua/RDSM/plugins/dashboard.lua
+++ lua/RDSM/plugins/dashboard.lua
@@ -2,7 +2,8 @@ local alpha = require("alpha")
 local db = require("alpha.themes.dashboard")
 local fortune = require("alpha.fortune")
 
-local plugins = #vim.tbl_keys(require("lazy").plugins())
+-- Adjust the position of the dashboard on screen
+local dbOffset = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) }
 
 -- Set header
 db.section.header.val = {
@@ -14,7 +15,16 @@ db.section.header.val = {
   "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
   "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
   "                                                     ",
-  "                      "..plugins.." plugins          ",
+}
+
+--Header description with a few information about nvim
+local headerDesc = {
+  type = "text",
+  val = "",
+  opts = {
+    position = "center",
+    hl = "String",
+  },
 }
 
 -- Set menu
@@ -30,12 +40,34 @@ db.section.buttons.val = {
 
 db.section.footer.val = fortune()
 
--- Adjust the position of the dashboard on screen
-db.config.layout[1].val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) }
--- db.config.layout[3].val = 5
+local opts = {
+  layout = {
+    { type = "padding", val = dbOffset },
+    db.section.header,
+    { type = "padding", val = 0 },
+    headerDesc,
+    { type = "padding", val = 2 },
+    db.section.buttons,
+    { type = "padding", val = 0 },
+    db.section.footer,
+  },
+  opts = {
+    margin = 44,
+  },
+}
 
 -- Send config of db to alpha
-alpha.setup(db.opts)
+alpha.setup(opts)
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'LazyVimStarted',
+	callback = function()
+		local stats = require('lazy').stats()
+		local ms = math.floor(stats.startuptime * 100 + 0.5) / 100
+		headerDesc.val = " Loaded "..stats.loaded.."/"..stats.count.." plugins in "..ms.."ms"
+		pcall(vim.cmd.AlphaRedraw)
+	end,
+})
 
 -- Disable folding on alpha buffer
 vim.cmd([[
