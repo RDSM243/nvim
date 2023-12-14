c61cc1d8b6964224254d3a7cb3a02dee9c8f7f5c
RDSM243 <didicomendes13@gmail.com>
2023-12-14 13:33:54 -0300

    Dashboard improvements

--- lua/RDSM/plugins/dashboard.lua
+++ lua/RDSM/plugins/dashboard.lua
@@ -1,5 +1,8 @@
 local alpha = require("alpha")
 local db = require("alpha.themes.dashboard")
+local fortune = require("alpha.fortune")
+
+local plugins = #vim.tbl_keys(require("lazy").plugins())
 
 -- Set header
 db.section.header.val = {
@@ -11,6 +14,7 @@ db.section.header.val = {
   "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
   "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
   "                                                     ",
+  "                      "..plugins.." plugins          ",
 }
 
 -- Set menu
@@ -24,9 +28,11 @@ db.section.buttons.val = {
     db.button( "q", "  Quit", ":qa<CR>"),
 }
 
+db.section.footer.val = fortune()
+
 -- Adjust the position of the dashboard on screen
 db.config.layout[1].val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) }
-db.config.layout[3].val = 5
+-- db.config.layout[3].val = 5
 
 -- Send config of db to alpha
 alpha.setup(db.opts)
